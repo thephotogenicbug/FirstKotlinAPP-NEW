73071d1fbe15197e3c360ad3910cd5e5860ed014
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-08 22:28:29 +0530

    Android LifeCycle

--- app/src/main/java/com/naveen/firstapp/MainActivity.kt
+++ app/src/main/java/com/naveen/firstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.naveen.firstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+         Log.i("MYTAG", "MainActivity : OnCreate")
          val greetingTextView = findViewById<TextView>(R.id.tvHello)
          val inputField = findViewById<EditText>(R.id.etName)
          val submitButton = findViewById<Button>(R.id.btnSubmit)
@@ -50,5 +51,35 @@ class MainActivity : AppCompatActivity() {
                intent.putExtra("USER", enteredName)
                startActivity(intent)
            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MYTAG", "MainActivity : OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MYTAG", "MainActivity : OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MYTAG", "MainActivity : OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MYTAG", "MainActivity : OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MYTAG", "MainActivity : OnDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MYTAG", "MainActivity : OnRestart")
     }
 }
